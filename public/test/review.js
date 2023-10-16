72d917056dde86f3be1262fdd4faa137cfd071d8
Hyunny <hazel413@naver.com>
2023-10-16 11:36:31 +0900

    [FEAT]map에서 idx 두번째 인자

--- public/test/review.js
+++ public/test/review.js
@@ -1,9 +1,9 @@
 //map grammar
 
 let arr=['a','b','c'];
-let list = arr.map(function(item){
+let list = arr.map(function(item, idx){
     let title = item +'항목';
-    return`<li>${title}</li>`;
+    return`<li key={item.idx}>${title}</li>`;
 })
 
 let list2 = arr.map((item)=>`<li>${item}</li>`);
--- src/components/LetterGrid.jsx
+++ src/components/LetterGrid.jsx
@@ -4,10 +4,10 @@ import Letter from './Letter';
 const LetterGrid = ({secretword, guessedLetters})=>{
     let lettered = secretword
                     .split('')
-                    .map((letter)=>{
+                    .map((letter,idx)=>{
                     //letter가 guessedLetters와 같다면 true = 1, false = -1
                     let isShown = guessedLetters.indexOf(letter.toLowerCase()) > -1;
-                    return(<Letter value={letter} isShown={isShown}/>);
+                    return(<Letter key={idx} value={letter} isShown={isShown}/>);
                 });
   console.log(lettered);
 
